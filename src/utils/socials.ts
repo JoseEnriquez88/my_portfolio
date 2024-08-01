c7e2d5a26fc9f58d704552038938be495b334bed
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 17:30:42 -0300

    socials added

---
+++ src/utils/socials.ts
@@ -0,0 +1,10 @@
+const socials = {
+  Location:
+    "https://www.google.com.ar/maps/place/Corrientes/@-27.4861694,-58.8335862,13z/data=!3m1!4b1!4m6!3m5!1s0x94456b79d5bed36b:0xfa999f1ef3b40646!8m2!3d-27.4692131!4d-58.8306349!16zL20vMDJ0Ymo1?entry=ttu",
+  Discord: "https://discord.com/channels/954778773654478940",
+  Github: "https://github.com/JoseEnriquez88",
+  Linkedin: "https://www.linkedin.com/in/joseenriquez80/",
+  Mail: "mailto:eenriquez.jose@gmail.com",
+};
+
+export default socials;
