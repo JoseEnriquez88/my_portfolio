21e5d93d7ace87160c1c9c48a16c51b2b5085c4e
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 17:30:57 -0300

    about motion added

---
+++ src/utils/motions/aboutMotion.ts
@@ -0,0 +1,68 @@
+export const titleVariants = {
+  initial: {
+    y: 0,
+    opacity: 0,
+  },
+  animate: {
+    y: -25,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+export const articleVariants = {
+  initial: {
+    y: 0,
+    opacity: 0,
+  },
+  animate: {
+    y: -30,
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+export const socialsVariants = {
+  initial: {
+    x: -30,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
