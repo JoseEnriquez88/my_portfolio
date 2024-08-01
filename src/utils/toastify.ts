266cc8c43cbd6d4c297a2caa2a17a6e0d91b9986
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 17:30:51 -0300

    toastify config added

---
+++ src/utils/toastify.ts
@@ -0,0 +1,53 @@
+import { toast } from "react-toastify";
+
+export const successNotify = () => {
+  toast.success("Gracias por comunicarte. Estaré en contacto pronto", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const errorNotify = () => {
+  toast.error("Hubo un error. Por favor intenta de nuevo", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const warnNotify = () => {
+  toast.warn("Asegúrate de haber completado todos los campos.", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const emailWarnNotify = () => {
+  toast.warn("Asegúrate de haber escrito un formato de email válido.", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
