8d69ca33d6dbde5fbdfdcf4435fefcf5fd21a8ed
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 17:29:59 -0300

    logos added

---
+++ src/utils/logos.ts
@@ -0,0 +1,17 @@
+const logos = [
+  "/logos/html.png",
+  "/logos/css.png",
+  "/logos/tailwind.png",
+  "/logos/mui.png",
+  "/logos/framer.png",
+  "/logos/js.png",
+  "/logos/react.png",
+  "/logos/next.png",
+  "/logos/redux.png",
+  "/logos/node-js.png",
+  "/logos/express.png",
+  "/logos/postgres.png",
+  "/logos/prisma.webp",
+];
+
+export default logos;
