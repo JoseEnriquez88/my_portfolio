eb399a486114941945522681c50819dcf01344ba
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 17:31:39 -0300

    items contact added

---
+++ src/utils/motions/itemsContactMotion.ts
@@ -0,0 +1,22 @@
+export const itemsVariants = {
+  initial: {
+    x: -25,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
