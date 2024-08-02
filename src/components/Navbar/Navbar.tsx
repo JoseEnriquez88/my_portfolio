b1d309f4efa98f54e44d78b56e4d26518a74ad0d
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 21:53:08 -0300

    Navbar + styles added

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import { Link } from "react-scroll";
+import Nav from "../NavLinks/Nav";
+import { useEffect, useState } from "react";
+
+const Navbar: React.FC = () => {
+  const [navbar, setNavbar] = useState<boolean>(false);
+
+  useEffect(() => {
+    const navColored = () => {
+      if (typeof window !== "undefined" && window.scrollY >= 950) {
+        setNavbar(true);
+      } else {
+        setNavbar(false);
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", navColored);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", navColored);
+      }
+    };
+  }, []);
+
+  return (
+    <header>
+      <nav
+        className={navbar ? `${styles.navbar} ${styles.active}` : styles.navbar}
+      >
+        <Link to="home">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            className={styles.logo}
+            width={50}
+            height={50}
+          />
+        </Link>
+        <div className={styles.itemsContainer}>
+          <Nav />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
