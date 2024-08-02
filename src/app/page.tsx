1088fbcd031f6ab7bb574a87ebe92e4bcec0b018
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 21:50:50 -0300

    page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { Fragment } from "react";
+import Hero from "@/components/Hero/Hero";
+import Navbar from "@/components/Navbar/Navbar";
+import Projects from "@/components/Projects/Projects";
+import About from "@/components/About/About";
+import Contact from "@/components/Contact/Contact";
+
 export default function Home() {
   return (
-    <>
-      <></>
-    </>
+    <Fragment>
+      <section id="home">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </Fragment>
   );
 }
