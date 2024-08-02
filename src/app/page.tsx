3fec42e13f1df6aa48659479c017bfa987e637c0
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-02 02:26:07 -0300

    page udpated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import { Fragment } from "react";
+"use client";
+import { Fragment, useState, useEffect } from "react";
+import Loader from "@/components/Loader/Loader";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 import Projects from "@/components/Projects/Projects";
@@ -6,21 +8,37 @@ import About from "@/components/About/About";
 import Contact from "@/components/Contact/Contact";
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Fragment>
-      <section id="home">
-        <Navbar />
-        <Hero />
-      </section>
-      <section id="projects">
-        <Projects />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <section id="home">
+            <Navbar />
+            <Hero />
+          </section>
+          <section id="projects">
+            <Projects />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          <section id="contact">
+            <Contact />
+          </section>
+        </Fragment>
+      )}
     </Fragment>
   );
 }
