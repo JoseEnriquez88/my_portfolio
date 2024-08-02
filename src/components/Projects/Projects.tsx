6b31bcf96278beb8e8d0c551bc1494006315508f
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 21:54:16 -0300

    projects component + styles added

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,32 @@
+"use client";
+import styles from "./projects.module.css";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+import { textVariants } from "@/utils/motions/projectsMotion";
+import CardsList from "../Cards/CardsList";
+
+const Projects: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref, { margin: "0px 0px -50% 0px" });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <motion.div
+          className={styles.textContainer}
+          variants={textVariants}
+          ref={ref}
+          animate={isInView && "animate"}
+        >
+          <h1 className={styles.title}>Proyectos</h1>
+          <h2>Ver mis últimos proyectos</h2>
+        </motion.div>
+        <div className={styles.projectContainer}>
+          <CardsList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
