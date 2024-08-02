98f89cae0c1efffd12c68a1f3c44b5a29a6e636c
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-01 21:52:57 -0300

    item contact + styles added

---
+++ src/components/ItemsContact/ItemsContact.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+import styles from "./itemsContact.module.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import socials from "@/utils/socials";
+import { itemsVariants } from "@/utils/motions/itemsContactMotion";
+
+const ItemsContact: React.FC = () => {
+  const ref = useRef<HTMLUListElement | null>(null);
+  const isInView = useInView(ref, { once: true, amount: 0.5 });
+
+  return (
+    <div className={styles.itemsCntnr}>
+      <motion.ul
+        className={styles.ulCntnr}
+        variants={itemsVariants}
+        initial="initial"
+        animate={isInView ? "animate" : "initial"}
+        ref={ref}
+      >
+        <a href={socials.Location} target="_blank" rel="noopener noreferrer">
+          <li>
+            <LocationOnIcon className={styles.icon} style={{ fontSize: 45 }} />
+            Corrientes, Corrientes. Argentina
+          </li>
+        </a>
+        <a href={socials.Linkedin} target="_blank" rel="noopener noreferrer">
+          <li>
+            <LinkedInIcon className={styles.icon} style={{ fontSize: 45 }} />
+            LinkedIn
+          </li>
+        </a>
+        <a href={socials.Mail} target="_blank" rel="noopener noreferrer">
+          <li>
+            <EmailIcon className={styles.icon} style={{ fontSize: 45 }} />
+            Email
+          </li>
+        </a>
+      </motion.ul>
+    </div>
+  );
+};
+
+export default ItemsContact;
