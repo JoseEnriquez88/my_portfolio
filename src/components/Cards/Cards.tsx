319d20015f099fcbd6f54978c1469fc6ea605e7a
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-08-13 16:42:02 -0300

    Cards component updated

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -3,7 +3,7 @@ import styles from "./cardslist.module.css";
 import Image from "next/image";
 import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
-import { Project, ProjectsData } from "@/utils/types";
+// import { Project, ProjectsData } from "@/utils/types";
 import {
   cardsContainerVariants,
   cardsVariants,
@@ -16,7 +16,7 @@ const Content: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(ref, { margin: "0px 0px -50% 0px" });
 
-  const projectData: ProjectsData = projects as ProjectsData;
+  // const projectData: ProjectsData = projects as ProjectsData;
 
   return (
     <motion.div
@@ -26,45 +26,96 @@ const Content: React.FC = () => {
       animate={isInView && "animate"}
       ref={ref}
     >
-      {projectData.data.map((project) => (
+      {projects.data.map((project, index) => (
         <motion.div
           className={styles.cardContainer}
-          key={project.id}
+          key={index}
           variants={cardsVariants}
         >
           <div className={styles.card}>
-            <div className={styles.frontContent}>
-              <Image src={project.image} alt="image" className={styles.image} width={500} height={350} />
-            </div>
-            <div className={styles.content}>
-              <p className={styles.heading}>{project.name}</p>
-              <p className={styles.type}>{project.type}</p>
-              <article className={styles.article}>
-                {project.description}
-              </article>
-              <div className={styles.iconContainer}>
-                <a
-                  href={project.repo}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.repoButton}
-                >
-                  Ver más
-                  <ArrowForwardIcon />
-                </a>
-                {project.deploy && (
-                  <a
-                    href={project.deploy}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.deployButton}
-                  >
-                    Deploy
-                    <InsertLinkIcon />
-                  </a>
-                )}
-              </div>
-            </div>
+            {index % 2 !== 0 ? (
+              <>
+                <div className={styles.frontContent}>
+                  <Image
+                    src={project.image}
+                    alt="image"
+                    className={styles.image}
+                    width={500}
+                    height={350}
+                  />
+                </div>
+                <div className={styles.content}>
+                  <p className={styles.heading}>{project.name}</p>
+                  <p className={styles.type}>{project.type}</p>
+                  <article className={styles.article}>
+                    {project.description}
+                  </article>
+                  <div className={styles.iconContainer}>
+                    <a
+                      href={project.repo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className={styles.repoButton}
+                    >
+                      Ver más
+                      <ArrowForwardIcon />
+                    </a>
+                    {project.deploy && (
+                      <a
+                        href={project.deploy}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.deployButton}
+                      >
+                        Deploy
+                        <InsertLinkIcon />
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className={styles.content}>
+                  <p className={styles.heading}>{project.name}</p>
+                  <p className={styles.type}>{project.type}</p>
+                  <article className={styles.article}>
+                    {project.description}
+                  </article>
+                  <div className={styles.iconContainer}>
+                    <a
+                      href={project.repo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className={styles.repoButton}
+                    >
+                      Ver más
+                      <ArrowForwardIcon />
+                    </a>
+                    {project.deploy && (
+                      <a
+                        href={project.deploy}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.deployButton}
+                      >
+                        Deploy
+                        <InsertLinkIcon />
+                      </a>
+                    )}
+                  </div>
+                </div>
+                <div className={styles.frontContent}>
+                  <Image
+                    src={project.image}
+                    alt="image"
+                    className={styles.image}
+                    width={500}
+                    height={350}
+                  />
+                </div>
+              </>
+            )}
           </div>
         </motion.div>
       ))}
