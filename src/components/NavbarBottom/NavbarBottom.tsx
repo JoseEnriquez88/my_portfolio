cfa437aef9f644c68aac210e69a4fc8513353e1e
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-02 06:54:05 -0300

    navbar bottom component updated

--- src/components/NavbarBottom/NavbarBottom.tsx
+++ src/components/NavbarBottom/NavbarBottom.tsx
@@ -1,77 +1,24 @@
-import styles from "./navbarBottom.module.css";
-import { useState } from "react";
-import { Link } from "react-scroll";
-// import HomeIcon from "@mui/icons-material/Home";
-// import WorkIcon from "@mui/icons-material/Work";
-// import PersonIcon from "@mui/icons-material/Person";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import IoMdContact from "@mui/icons-material/ContactPage";
-import {
-  IoMdHome,
-  IoMdBriefcase,
-  IoMdPersonAdd,
-  IoMdContact,
-  IoIosMenu,
-} from "react-icons/io";
-import { SiAboutdotme } from "react-icons/si"; //! se puede usar en el about me
-import SocialsLinks from "../SocialLinks/SocialLinks";
+"use client";
+import { useState, useLayoutEffect } from "react";
+import BottomNav from "./BottomNav/BottomNav";
 
 const NavbarBottom: React.FC = () => {
-  const [socialLinksOpen, setSocialLinksOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
-  const handleMenuClick = () => {
-    setSocialLinksOpen(true);
-  };
+  useLayoutEffect(() => {
+    const showNav = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
 
-  const handleCloseSocialLinks = () => {
-    setSocialLinksOpen(false);
-  };
+    showNav();
+    window.addEventListener("resize", showNav);
 
-  return (
-    <div className={styles.container}>
-      <ul className={styles.iconContainer}>
-        <Link to="home">
-          <li>
-            <span role="img">
-              <IoMdHome className={styles.icon} />
-            </span>
-            <span className={styles.title}>Inicio</span>
-          </li>
-        </Link>
-        <Link to="projects">
-          <li>
-            <span role="img">
-              <IoMdBriefcase className={styles.icon} />
-            </span>
-            Proyectos
-          </li>
-        </Link>
-        <Link to="about">
-          <li>
-            <span role="img">
-              <IoMdPersonAdd className={styles.icon} />
-            </span>
-            Sobre mí
-          </li>
-        </Link>
-        <Link to="contact">
-          <li>
-            <span role="img">
-              <IoMdContact className={styles.icon} />
-            </span>
-            Contacto
-          </li>
-        </Link>
-        <li onClick={handleMenuClick}>
-          <span role="img">
-            <IoIosMenu className={styles.icon} />
-          </span>
-          Redes
-        </li>
-      </ul>
-      <SocialsLinks open={socialLinksOpen} onClose={handleCloseSocialLinks} />
-    </div>
-  );
+    return () => {
+      window.removeEventListener("resize", showNav);
+    };
+  }, []);
+
+  return <>{isMobile && <BottomNav />}</>;
 };
 
 export default NavbarBottom;
