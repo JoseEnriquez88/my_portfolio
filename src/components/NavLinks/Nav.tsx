d9b3f93d94d5e7c7d943cbd5e40bc316617c2a2e
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-02 07:25:59 -0300

    nav component updated

--- src/components/NavLinks/Nav.tsx
+++ src/components/NavLinks/Nav.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
 import styles from "./navlinks.module.css";
+import React from "react";
+import { Link } from "react-scroll";
 
 const pillTabs = [
   { title: "Inicio", href: "home" },
@@ -10,48 +11,19 @@ const pillTabs = [
 ];
 
 const Nav: React.FC = () => {
-  useEffect(() => {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("header nav a");
-
-    const handleScroll = () => {
-      sections.forEach((sec) => {
-        const top = window.scrollY;
-        const offset = sec.offsetTop - 150;
-        const height = sec.offsetHeight;
-        const id = sec.getAttribute("id");
-
-        if (top >= offset && top < offset + height) {
-          navLinks.forEach((link) => {
-            link.classList.remove(styles.active);
-            const activeLink = document.querySelector(
-              `header nav a[href*="${id}"]`
-            );
-            if (activeLink) {
-              activeLink.classList.add(styles.active);
-            }
-          });
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <header className={styles.container}>
       <nav className={styles.nav}>
         {pillTabs.map((tab, index) => (
-          <a
+          <Link
             key={index}
-            href={`#${tab.href}`}
-            className={index === 0 ? styles.active : ""}
+            to={tab.href}
+            spy={true}
+            smooth={true}
+            activeClass={styles.active}
           >
             {tab.title}
-          </a>
+          </Link>
         ))}
       </nav>
     </header>
