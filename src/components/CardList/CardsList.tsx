9ffbe00524dbef601a571b7e4d68ef6f2395acc5
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-02 06:54:18 -0300

    cards list component updated

---
+++ src/components/CardList/CardsList.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useState, useEffect } from "react";
+import Content from "./Content/Content";
+import Cards from "./Cards/Cards";
+
+const CardsList: React.FC = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <>{isMobile ? <Content /> : <Cards />}</>;
+};
+
+export default CardsList;
