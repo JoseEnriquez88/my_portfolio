7348f0baaee3e7936bc951d7a7915fc54799ce11
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-24 22:34:59 -0300

    waves framer motion config  component was added

---
+++ src/utils/motions/wavesMotion.ts
@@ -0,0 +1,14 @@
+export const svgVariants = {
+  hidden: {
+    y: "100vw",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+      ease: "easeOut",
+    },
+  },
+};
