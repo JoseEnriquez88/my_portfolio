52008d575993a415ba74588c2666addd308b7a98
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-24 23:55:43 -0300

    layout was updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/svg+xml" href="./logo.svg" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
       </head>
       <body> {children}</body>
     </html>
