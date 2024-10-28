95824762e62d78960672487c337bf5d22d384731
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-10-28 00:57:49 -0300

    nav component was updated

---
+++ src/components/NavLinks/Nav/Nav.tsx
@@ -0,0 +1,35 @@
+"use client";
+import styles from "./nav.module.css";
+import React from "react";
+import { Link } from "react-scroll";
+
+const pillTabs = [
+  { title: "Inicio", href: "home" },
+  { title: "Proyectos", href: "projects" },
+  { title: "Sobre mí", href: "about" },
+  { title: "Contacto", href: "contact" },
+];
+
+const Nav: React.FC = () => {
+  return (
+    <header className={styles.container}>
+      <nav className={styles.nav}>
+        {pillTabs.map((tab, index) => (
+          <Link
+            key={index}
+            to={tab.href}
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={100}
+            activeClass={styles.active}
+          >
+            {tab.title}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
