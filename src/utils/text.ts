3751b7a69a75f5cbc6dab3f261f694f1c6bb9679
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-01-30 01:52:49 -0300

    text was added

---
+++ src/utils/text.ts
@@ -0,0 +1,7 @@
+export const aboutBio: string = `Soy un desarrollador web con fuertes habilidades en HTML, CSS
+                  y JavaScript, lo que me permite crear interfaces atractivas.
+                  Además, tengo experiencia desarrolando con tecnologías como
+                  React, React Native, Next y Redux/Toolkit. Del lado del
+                  servidor, utilizo Express y bases de datos relacionales como
+                  SQL, Prisma y PostgreSQL para garantizar un rendimiento
+                  eficiente y una gestión de datos eficaz.`;
